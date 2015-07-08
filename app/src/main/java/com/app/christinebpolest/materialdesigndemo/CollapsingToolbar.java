2f5966ab840a2cee6045130ed2527eb1f01fb742
xtinepols <xtinepolestico@gmail.com>
2015-07-09 00:23:38 +0800

    fourth

--- app/src/main/java/com/app/christinebpolest/materialdesigndemo/CollapsingToolbar.java
+++ app/src/main/java/com/app/christinebpolest/materialdesigndemo/CollapsingToolbar.java
@@ -1,30 +1,42 @@
 package com.app.christinebpolest.materialdesigndemo;
 
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Interpolator;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.text.TextPaint;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.jar.Attributes;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CollapsingToolbar extends Fragment {
 
+    View view;
 
     public CollapsingToolbar() {
-        // Required empty public constructor
     }
 
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collapsing_toolbar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_collapsing_toolbar, container, false);
+        CollapsingToolbar collapsingToolbar = (CollapsingToolbar)view.findViewById(R.id.seventh_collapsingToolbar);
+        collapsingToolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hey! You're clicking floating action button", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
-
-
 }
--- app/src/main/java/com/app/christinebpolest/materialdesigndemo/MainActivity.java
+++ app/src/main/java/com/app/christinebpolest/materialdesigndemo/MainActivity.java
@@ -1,126 +1,111 @@
 package com.app.christinebpolest.materialdesigndemo;
 
-import android.app.ActionBar;
-import android.support.design.widget.FloatingActionButton;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         final ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.header);
+        actionBar.setHomeAsUpIndicator(android.R.drawable.btn_plus);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        fragmentManager = getSupportFragmentManager();
 
-        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        navigationView = (NavigationView)findViewById(R.id.navigationView);
         if(navigationView != null) {
-            setUpDrawerContent(navigationView);
+            setupDrawerContent(navigationView);
         }
-
-        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
-        if(viewPager != null) {
-            setUpViewPager(viewPager);
-        }
-
-        FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(R.id.fab);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Hey! Keep smiling :)", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
-
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
+    public void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.first_navigationTB:
+                fragmentClass = MainActivity.class;
+                break;
+            case R.id.second_fab:
+                fragmentClass = fab.class;
+                break;
+            case R.id.third_snackbar:
+                fragmentClass = SnackBar.class;
+                break;
+            case R.id.sixth_coordinatorAppBarLayout:
+                fragmentClass = CoordinatorAppbar.class;
+                break;
+            case R.id.seventh_collapsingToolbar:
+                fragmentClass = CollapsingToolbar.class;
+                break;
+            default:
+                fragmentClass = MainActivity.class;
+        }
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawers();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.sample_actions, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void setUpViewPager(ViewPager viewPager) {
-        Adapter adapter = new Adapter(getSupportFragmentManager());
-//        adapter.addFragment(new CheeFrag(), "Category 1");
-        viewPager.setAdapter(adapter);
-    }
-
-    private void setUpDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                menuItem.setChecked(true);
-                mDrawerLayout.closeDrawers();
-                return true;
-            }
-        });
-    }
-
-    static class Adapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragments = new ArrayList<>();
-        private final List<String> mFragmentTitles = new ArrayList<>();
-
-        public Adapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragments.add(fragment);
-            mFragmentTitles.add(title);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragments.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragments.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitles.get(position);
-        }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 }
